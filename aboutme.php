c921a1ddac9c4f136c17513d621864741f617948
anyefremova <anna.efremova.02@yandex.ru>
2023-05-25 19:32:36 +0500

    добавила иконки и php файлы

---
+++ aboutme.php
@@ -0,0 +1,66 @@
+<section class="main__block-4 block-4">
+  <div class="container">
+        <h5 class="card-title title-block-4">Обо мне</h5>
+        <div class="card mb-lg-0 border-0" style="max-width: 1500px;">
+          <div class="row g-0">
+            <div class="col-md-6">
+              <img src="assets/images/b4_aboutme_img.JPG" class="img-fluid rounded-start block4__img" alt="...">
+            </div>
+            <div class="col-md-6">
+              <div class="block-4__card-body">
+                <div class="card-body__title">
+                  <h5 class="card-title__block-4">Мария Кукиева</h5>
+                  <h6 class="card-subtitle__block-4">English teacher</h6>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item d-flex align-items-center border-0">
+                    <img src="assets/icons/icon _arrow circle right_.svg" class="bi bi-arrow-right-circle"
+                      alt="icon arrow circle right">
+                    Лингвист-переводчик, Уральский Федеральный Университет (УрФУ), Физико-технический факультет,
+                    Кафедра
+                    иностранных языков, г. Екатеринбург (диплом с отличием)
+                  </li>
+                  <li class="list-group-item d-flex align-items-center border-0">
+                    <img src="assets/icons/icon _arrow circle right_.svg" class="bi bi-arrow-right-circle"
+                      alt="icon arrow circle right">
+                    Международные сертификаты по методике преподавания  - Cambridge CELTA, TKT
+                  </li>
+                  <li class="list-group-item d-flex align-items-center border-0">
+                    <img src="assets/icons/icon _arrow circle right_.svg" class="bi bi-arrow-right-circle"
+                      alt="icon arrow circle right">
+                    CPE (C2 English Language Proficiency)
+                  </li>
+                  <li class="list-group-item d-flex align-items-center border-0">
+                    <img src="assets/icons/icon _arrow circle right_.svg" class="bi bi-arrow-right-circle"
+                      alt="icon arrow circle right">
+                    10 лет преподаю английский язык, из них 9 лет готовлю студентов к IELTS:
+                  </li>
+                <ul class="list-group">
+                  <li class="list-group-item d-flex align-items-center border-0">
+                  <img src="assets/icons/dot.svg" alt="dot">
+                    УрФУ - подготовка ППС (профессорско-преподавательский состав) к Кембриджским экзаменам KET и FCE
+                  </li>
+                  <li class="list-group-item d-flex align-items-center border-0">
+                  <img src="assets/icons/dot.svg" alt="dot">
+                    Международный экзаменационный центр English Lab - группы по подготовке к IELTS
+                  </li>
+                  <li class="list-group-item d-flex align-items-center border-0">
+                  <img src="assets/icons/dot.svg" alt="dot">
+                    Тьютор на курсе Академического письма (формат IELTS)
+                  </li>
+                  <li class="list-group-item d-flex align-items-center border-0">
+                  <img src="assets/icons/dot.svg" alt="dot">
+                    Создала свой курс по написанию писем - IELTS General (Writing Task 1)
+                  </li>
+                  <li class="list-group-item d-flex align-items-center border-0">
+                  <img src="assets/icons/dot.svg" alt="dot">
+                    Консультирую преподавателей по вопросам подготовки к IELTS
+                  </li>
+                </ul>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+  </div>
+</section>
--- index.php
+++ index.php
@@ -150,67 +150,35 @@
       </div>
     </section>
 
-    <section class="main__block-4 block-4">
+    <?php require_once ('aboutme.php')?>
+
+    <section class="main__block-5 block-5">
       <div class="container">
-        <h5 class="card-title title-block-4">Обо мне</h5>
-        <div class="card mb-lg-0 border-0" style="max-width: 1500px;">
-          <div class="row g-0">
-            <div class="col-md-6">
-              <img src="assets/images/b4_aboutme_img.JPG" class="img-fluid rounded-start" alt="...">
+        <h5 class="card-title fs-1">Стоимость занятий</h5>
+        <div class="row">
+          <div class="col-4">
+            <div class="card-body">
+              <h5 class="card-title">Индивидуально</h5>
+              <p class="card-text">Подготовка к IELTS. Академическое письмо (формат IELTS). Общий английский</p>
+              <p class="card-text">Стоимость: 5000 р</p>
+              <button class="btn btn-outline-primary" type="submit">Связаться</button>
             </div>
-            <div class="col-md-6">
-              <div class="block-4__card-body">
-                <div class="card-body__title">
-                  <h5 class="card-title__block-4">Мария Кукиева</h5>
-                  <h6 class="card-subtitle__block-4">English teacher</h6>
-                </div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item d-flex align-items-center border-0">
-                    <img src="assets/icons/icon _arrow circle right_.svg" class="bi bi-arrow-right-circle"
-                      alt="icon arrow circle right">
-                    Лингвист-переводчик, Уральский Федеральный Университет (УрФУ), Физико-технический факультет,
-                    Кафедра
-                    иностранных языков, г. Екатеринбург (диплом с отличием)
-                  </li>
-                  <li class="list-group-item d-flex align-items-center border-0">
-                    <img src="assets/icons/icon _arrow circle right_.svg" class="bi bi-arrow-right-circle"
-                      alt="icon arrow circle right">
-                    Международные сертификаты по методике преподавания  - Cambridge CELTA, TKT
-                  </li>
-                  <li class="list-group-item d-flex align-items-center border-0">
-                    <img src="assets/icons/icon _arrow circle right_.svg" class="bi bi-arrow-right-circle"
-                      alt="icon arrow circle right">
-                    CPE (C2 English Language Proficiency)
-                  </li>
-                  <li class="list-group-item d-flex align-items-center border-0">
-                    <img src="assets/icons/icon _arrow circle right_.svg" class="bi bi-arrow-right-circle"
-                      alt="icon arrow circle right">
-                    10 лет преподаю английский язык, из них 9 лет готовлю студентов к IELTS:
-                  </li>
-                  <ul class="list-group">
-                    <li class="list-group-item d-flex align-items-center border-0">
-                      <img src="assets/icons/dot.svg" alt="dot">
-                      УрФУ - подготовка ППС (профессорско-преподавательский состав) к Кембриджским экзаменам KET и FCE
-                    </li>
-                    <li class="list-group-item d-flex align-items-center border-0">
-                      <img src="assets/icons/dot.svg" alt="dot">
-                      Международный экзаменационный центр English Lab - группы по подготовке к IELTS
-                    </li>
-                    <li class="list-group-item d-flex align-items-center border-0">
-                      <img src="assets/icons/dot.svg" alt="dot">
-                      Тьютор на курсе Академического письма (формат IELTS)
-                    </li>
-                    <li class="list-group-item d-flex align-items-center border-0">
-                      <img src="assets/icons/dot.svg" alt="dot">
-                      Создала свой курс по написанию писем - IELTS General (Writing Task 1)
-                    </li>
-                    <li class="list-group-item d-flex align-items-center border-0">
-                      <img src="assets/icons/dot.svg" alt="dot">
-                      Консультирую преподавателей по вопросам подготовки к IELTS
-                    </li>
-                  </ul>
-                </ul>
-              </div>
+          </div>
+          <div class="col-4">
+            <div class="card-body">
+              <h5 class="card-title">Консультации</h5>
+              <p class="card-text">Подготовка к IELTS. Академическое письмо (формат IELTS). Общий английский</p>
+              <p class="card-text">Стоимость: 5000 р</p>
+              <button class="btn btn-outline-primary" type="submit">Связаться</button>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card-body">
+              <h5 class="card-title">Консультации</h5>
+              <p class="card-text">Консультации для преподавателей Консультация по формату IELTS Консультация по
+                вопросам подготовки к IELTS</p>
+              <p class="card-text">Стоимость: 5000 р</p>
+              <button class="btn btn-outline-primary" type="submit">Связаться</button>
             </div>
           </div>
         </div>
