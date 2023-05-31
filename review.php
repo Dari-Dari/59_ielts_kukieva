f4acd3512f16328b68a77f5a7f465542c15411d9
unknown <valennntina220@gmail.com>
2023-05-31 19:54:29 +0300

    скрыты кнопки

--- review.php
+++ review.php
@@ -1,5 +1,3 @@
-<?php require_once('index.php') ?>
-
 <section class="main__block-6 block-6">
             <div class="container">
               <h5 class="card-title fs-1 title__block-6">Отзывы</h5>
@@ -31,9 +29,9 @@
                     <div class="card-wrapper__block-6">
                       <div class="card card__block-6" style="width: 18rem">
                         <img
-                          src="/59_ielts_kukieva/assets/images/2-Golyakova.jpg"
+                          src="/assets/images/2-Golyakova.jpg"
                           class="card-img-top img__block-6"
-                          alt=""
+                          alt="image"
                         />
                         <div class="card-body">
                           <p class="card-text text__block-6">
@@ -45,9 +43,9 @@
                       </div>
                       <div class="card card__block-6" style="width: 18rem">
                         <img
-                          src="/59_ielts_kukieva/assets/images/3-Koltsova.jpg"
+                          src="/assets/images/3-Koltsova.jpg"
                           class="card-img-top img__block-6"
-                          alt=""
+                          alt="image"
                         />
                         <div class="card-body">
                           <p class="card-text text__block-6">
@@ -59,9 +57,9 @@
                       </div>
                       <div class="card card__block-6" style="width: 18rem">
                         <img
-                          src="/59_ielts_kukieva/assets/images/4-batekin.jpg"
+                          src="/assets/images/4-batekin.jpg"
                           class="card-img-top img__block-6"
-                          alt=""
+                          alt="image"
                         />
                         <div class="card-body">
                           <p class="card-text text__block-6">
@@ -77,9 +75,9 @@
                     <div class="card-wrapper__block-6">
                       <div class="card card__block-6" style="width: 18rem">
                         <img
-                          src="/59_ielts_kukieva/assets/images/1-1Ivanova.jpg"
+                          src="/assets/images/1-1Ivanova.jpg"
                           class="card-img-top img__block-6"
-                          alt=""
+                          alt="image"
                         />
                         <div class="card-body">
                           <p class="card-text">
@@ -87,7 +85,7 @@
                         </div>
                       </div>
                       <div class="card card__block-6" style="width: 18rem">
-                        <img src="/59_ielts_kukieva/assets/images/1-2Ivanova.jpg" class="card-img-top img__block-6" alt="" />
+                        <img src="/assets/images/1-2Ivanova.jpg" class="card-img-top img__block-6" alt="image" />
                         <div class="card-body">
                           <p class="card-text">
                            
@@ -95,7 +93,7 @@
                         </div>
                       </div>
                       <div class="card card__block-6" style="width: 18rem">
-                        <img src="/59_ielts_kukieva/assets/images/1-3Ivanova.jpg" class="card-img-top img__block-6" alt="" />
+                        <img src="/assets/images/1-3Ivanova.jpg" class="card-img-top img__block-6" alt="image" />
                         <div class="card-body">
                           <p class="card-text text__block-6">
                           Ангелина Иванова, журналист, г.Токио, Япония <br>
@@ -109,7 +107,7 @@
                   <div class="carousel-item">
                     <div class="card-wrapper__block-6">
                       <div class="card card__block-6" style="width: 18rem">
-                        <img src="/59_ielts_kukieva/assets/images/6-Kulakova.jpg" class="card-img-top img__block-6" alt="" />
+                        <img src="/assets/images/6-Kulakova.jpg" class="card-img-top img__block-6" alt="image" />
                         <div class="card-body">
                           <p class="card-text text__block-6">
                           Юлия Кулакова, архитектор, г.Окленд, Новая Зеландия <br>
@@ -119,7 +117,7 @@
                         </div>
                       </div>
                       <div class="card card__block-6" style="width: 18rem">
-                        <img src="/59_ielts_kukieva/assets/images/7-Trunov.jpg" class="card-img-top img__block-6" alt="" />
+                        <img src="/assets/images/7-Trunov.jpg" class="card-img-top img__block-6" alt="image" />
                         <div class="card-body">
                           <p class="card-text text__block-6">
                           Георгий Трунов, студент Университета прикладных наук Ханзе (Hanze UAS), г.Гронинген, Нидерланды <br>
@@ -128,7 +126,7 @@
                         </div>
                       </div>
                       <div class="card card__block-6" style="width: 18rem">
-                        <img src="/59_ielts_kukieva/assets/images/10-1Vaginskaya.jpg" class="card-img-top img__block-6" alt="" />
+                        <img src="/images/10-1Vaginskaya.jpg" class="card-img-top img__block-6" alt="image" />
                         <div class="card-body">
                           <p class="card-text text__block-6">
                           Валерия Важинская, учитель английского языка <br>
@@ -141,7 +139,7 @@
                   </div>
                 </div>
                 <button
-                  class="carousel-control-prev"
+                  class="carousel-control-prev d-none d-md-block"
                   type="button"
                   data-bs-target="#carouselExampleDark"
                   data-bs-slide="prev"
@@ -150,7 +148,7 @@
                   <span class="visually-hidden">Previous</span>
                 </button>
                 <button
-                  class="carousel-control-next"
+                  class="carousel-control-next d-none d-md-block"
                   type="button"
                   data-bs-target="#carouselExampleDark"
                   data-bs-slide="next"
@@ -163,47 +161,47 @@
 
               <div id="carouselExampleIndicatorsSmallScreen" class="carousel slide d-sm-none">
                 <div class="carousel-indicators">
-                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
-                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
-                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
-                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="7" aria-label="Slide 8"></button>
-                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="8" aria-label="Slide 9"></button>
-
+                  <button type="button" data-bs-target="#carouselExampleIndicatorsSmallScreen" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicatorsSmallScreen" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicatorsSmallScreen" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicatorsSmallScreen" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicatorsSmallScreen" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicatorsSmallScreen" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicatorsSmallScreen" data-bs-slide-to="6" aria-label="Slide 7"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicatorsSmallScreen" data-bs-slide-to="7" aria-label="Slide 8"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicatorsSmallScreen" data-bs-slide-to="8" aria-label="Slide 9"></button>
                 </div>
                 <div class="carousel-inner">
                   <div class="carousel-item active">
                     <div class="card-wrapper__block-6">
                       <div class="card card__block-6" style="width: 18rem">
                         <img
-                          src="/59_ielts_kukieva/assets/images/2-Golyakova.jpg"
+                          src="/assets/images/2-Golyakova.jpg"
                           class="card-img-top img__block-6"
-                          alt=""
+                          alt="image"
                         />
                         <div class="card-body">
                           <p class="card-text text__block-6">
-                            Наталия Голякова, менеджер проектов, г.Ванкувер, Канада <br>
-                            @nicoll_99 <br />
+                            Наталия Голякова, менеджер проектов, г.Ванкувер, Канада
+                            @nicoll_99
                             Индивидуальная подготовка к IELTS
                           </p>
                         </div>
+                      </div>
                       </div>
                   </div>
                   <div class="carousel-item">
                     <div class="card-wrapper__block-6">
                         <div class="card card__block-6" style="width: 18rem">
                             <img
-                              src="/59_ielts_kukieva/assets/images/3-Koltsova.jpg"
+                              src="/assets/images/3-Koltsova.jpg"
                               class="card-img-top img__block-6"
-                              alt=""
+                              alt="image"
                             />
                             <div class="card-body">
                               <p class="card-text text__block-6">
-                              Мария Кольцова, журналист, г.Тбилиси, Грузия <br>
-          @kolt_mi <br>
+                              Мария Кольцова, журналист, г.Тбилиси, Грузия
+          @kolt_mi 
           Индивидуальная подготовка к IELTS
                               </p>
                             </div>
@@ -214,27 +212,27 @@
                     <div class="card-wrapper__block-6">
                         <div class="card card__block-6" style="width: 18rem">
                             <img
-                              src="/59_ielts_kukieva/assets/images/4-batekin.jpg"
+                              src="/assets/images/4-batekin.jpg"
                               class="card-img-top img__block-6"
-                              alt=""
+                              alt="image"
                             />
                             <div class="card-body">
                               <p class="card-text text__block-6">
-                              Станислав Батекин, инженер, г.Аделаида, Австралия. <br>
-          @sbatekin <br>
-          Индивидуальная подготовка к IELTS <br>
+                              Станислав Батекин, инженер, г.Аделаида, Австралия.
+          @sbatekin 
+          Индивидуальная подготовка к IELTS 
                               </p>
                             </div>
                           </div>
                       </div>
                   </div>
-                  <div class="carousel-item active">
+                  <div class="carousel-item">
                     <div class="card-wrapper__block-6">
                         <div class="card card__block-6" style="width: 18rem">
                             <img
-                              src="/59_ielts_kukieva/assets/images/1-1 Ivanova.jpg"
+                              src="/assets/images/1-1Ivanova.jpg"
                               class="card-img-top img__block-6"
-                              alt=""
+                              alt="image"
                             />
                             <div class="card-body">
                               <p class="card-text">
@@ -243,10 +241,10 @@
                          </div>
                       </div>
                   </div>
-                  <div class="carousel-item active">
+                  <div class="carousel-item">
                     <div class="card-wrapper__block-6">
                         <div class="card card__block-6" style="width: 18rem">
-                            <img src="/59_ielts_kukieva/assets/images/1-2 Ivanova.jpg" class="card-img-top img__block-6" alt="" />
+                            <img src="/assets/images/1-2Ivanova.jpg" class="card-img-top img__block-6" alt="image" />
                             <div class="card-body">
                               <p class="card-text">
                               </p>
@@ -254,55 +252,55 @@
                           </div>
                     </div>
                   </div>
-                  <div class="carousel-item active">
+                  <div class="carousel-item">
                     <div class="card-wrapper__block-6">
                         <div class="card card__block-6" style="width: 18rem">
-                            <img src="/59_ielts_kukieva/assets/images/1-3 Ivanova.jpg" class="card-img-top img__block-6" alt="" />
+                            <img src="/assets/images/1-3Ivanova.jpg" class="card-img-top img__block-6" alt="image" />
                             <div class="card-body">
                               <p class="card-text text__block-6">
-                              Ангелина Иванова, журналист, г.Токио, Япония <br>
-          @koalified.coach <br>
-          Индивидуальная подготовка к IELTS <br>
+                              Ангелина Иванова, журналист, г.Токио, Япония
+          @koalified.coach 
+          Индивидуальная подготовка к IELTS
                               </p>
                             </div>
                          </div>
                     </div>
                   </div>
-                  <div class="carousel-item active">
+                  <div class="carousel-item">
                     <div class="card-wrapper__block-6">
                         <div class="card card__block-6" style="width: 18rem">
-                            <img src="/59_ielts_kukieva/assets/images/6-Kulakova.jpg" class="card-img-top img__block-6" alt="" />
+                            <img src="/assets/images/6-Kulakova.jpg" class="card-img-top img__block-6" alt="image" />
                             <div class="card-body">
                               <p class="card-text text__block-6">
-                              Юлия Кулакова, архитектор, г.Окленд, Новая Зеландия <br>
-          @_julia_alikina <br>
-          Групповая подготовка к IELTS <br>
+                              Юлия Кулакова, архитектор, г.Окленд, Новая Зеландия 
+          @_julia_alikina 
+          Групповая подготовка к IELTS 
                               </p>
                             </div>
                           </div>
                     </div>
                   </div>
-                  <div class="carousel-item active">
+                  <div class="carousel-item">
                     <div class="card-wrapper__block-6">
                         <div class="card card__block-6" style="width: 18rem">
-                            <img src="/59_ielts_kukieva/assets/images/7-Trunov.jpg" class="card-img-top img__block-6" alt="" />
+                            <img src="/assets/images/7-Trunov.jpg" class="card-img-top img__block-6" alt="image" />
                             <div class="card-body">
                               <p class="card-text text__block-6">
-                              Георгий Трунов, студент Университета прикладных наук Ханзе (Hanze UAS), г.Гронинген, Нидерланды <br>
+                              Георгий Трунов, студент Университета прикладных наук Ханзе (Hanze UAS), г.Гронинген, Нидерланды
           Консультация по формату IELTS
                               </p>
                             </div>
                           </div>
                     </div>
                   </div>
-                  <div class="carousel-item active">
+                  <div class="carousel-item">
                     <div class="card-wrapper__block-6">
                         <div class="card card__block-6" style="width: 18rem">
-                            <img src="/59_ielts_kukieva/assets/images/10-1 Vaginskaya.jpg" class="card-img-top img__block-6" alt="" />
+                            <img src="/assets/images/10-1Vaginskaya.jpg" class="card-img-top img__block-6" alt="image" />
                             <div class="card-body">
                               <p class="card-text text__block-6">
-                              Валерия Важинская, учитель английского языка <br>
-          @leranimess <br>
+                              Валерия Важинская, учитель английского языка 
+          @leranimess 
           Консультация для преподавателей по вопросам подготовки к IELTS
                               </p>
                             </div>
@@ -310,12 +308,12 @@
                     </div>
                   </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicatorsSmallScreen" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <button class="carousel-control-prev visually-hidden" type="button" data-bs-target="#carouselExampleIndicatorsSmallScreen" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon visually-hidden" aria-hidden="true"></span>
                   <span class="visually-hidden">Previous</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicatorsSmallScreen" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <button class="carousel-control-next visually-hidden" type="button" data-bs-target="#carouselExampleIndicatorsSmallScreen" data-bs-slide="next">
+                  <span class="carousel-control-next-icon visually-hidden" aria-hidden="true"></span>
                   <span class="visually-hidden">Next</span>
                 </button>
               </div>
